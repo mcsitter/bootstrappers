97dcc55395191065a9fe1fe36d7c65e87f0b9ed7
mcsitter <48606431+mcsitter@users.noreply.github.com>
2020-05-31 16:48:18 +0200

    Test running as admin

---
+++ windows/bootstrap.ps1
@@ -0,0 +1,11 @@
+# Validation
+function Test-Elevated {
+    $myIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $myPrincipal = New-Object System.Security.Principal.WindowsPrincipal ($myIdentity)
+    return $myPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
+if (!(Test-Elevated)) {
+    Write-Host "Not running as Administrator"
+    exit
+}
