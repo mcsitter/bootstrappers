f8a0a8743b896ecab057c4227041a751163c5768
mcsitter <48606431+mcsitter@users.noreply.github.com>
2020-06-04 13:20:51 +0200

    Refreshenv after chocolatey install

--- windows/bootstrap.ps1
+++ windows/bootstrap.ps1
@@ -12,9 +12,17 @@ if (!(Test-Elevated)) {
 
 # Install Chocolatey
 if (!(Get-Command "choco.exe" -ErrorAction SilentlyContinue)) {
+    Write-Host "Installing Chocolatey"
     Set-ExecutionPolicy Bypass -Scope Process -Force
-    $chocoInstaller = Invoke-WebRequest 'https://chocolatey.org/install.ps1' -UseBasicParsing
+    $chocoInstaller = Invoke-WebRequest "https://chocolatey.org/install.ps1" -UseBasicParsing
     Invoke-Expression $chocoInstaller
+
+    # Make `refreshenv` available right away, by defining the $env:ChocolateyInstall variable
+    # and importing the Chocolatey profile module.
+    $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).path)\..\.."
+    Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+
+    RefreshEnv.cmd
 } else {
     $chocoVersion = choco --version
     Write-Host "chocolatey v$chocoVersion already installed."
